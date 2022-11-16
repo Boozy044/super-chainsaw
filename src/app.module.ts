10cef136a78275f3729a697683e12e7cb71366fa
Boozy044 <boozy044@gmail.com>
2022-11-16 16:40:07 +0000

    Generate auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule
+    DatabaseModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
