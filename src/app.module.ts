2be86ffddbefe2237432d1b92b34b4d0a44a78bf
Boozy044 <boozy044@gmail.com>
2022-11-16 13:13:35 +0000

    Initialize config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true })
+  ],
   controllers: [],
   providers: [],
 })
