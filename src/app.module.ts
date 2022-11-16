cdf6bb213ed510ef94f24a82baec2f51f97de283
Boozy044 <boozy044@gmail.com>
2022-11-16 13:59:50 +0000

    Setup settings database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './core/database/database.module';
 
 @Module({
   imports: [
-    DatabaseModule,
-    ConfigModule.forRoot({ isGlobal: true })
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule
   ],
   controllers: [],
   providers: [],
